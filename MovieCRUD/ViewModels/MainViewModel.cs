debda596ee240838a36468c53b63aa6722405a8b
Gessio Mori Neto <gessiomorin@gmail.com>
2023-03-08 08:31:46 -0300

    Refactors to use RelayCommand

--- MovieCRUD/ViewModels/MainViewModel.cs
+++ MovieCRUD/ViewModels/MainViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using MovieCRUD.Models;
 using MovieCRUD.ViewModels.Commands;
+using MovieCRUD.Views.Windows;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
@@ -7,6 +8,7 @@ using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace MovieCRUD.ViewModels
 {
@@ -14,15 +16,12 @@ namespace MovieCRUD.ViewModels
 
     {
         public ObservableCollection<Director> Directors { get; private set; }
-       
+
         private Director? _selectedDirector;
 
-        public DeleteDirectorCommand DeleteDirectorCommand { get; private set; } = new DeleteDirectorCommand();
-
-        public AddDirectorCommand AddDirectorCommand { get; private set; }
-
-        // Nome da propriedade?
-        public UpdateDirectorCommand UpdateDirectorCommand { get; private set; }
+        public ICommand? DeleteDirectorCommand { get; private set; }
+        public ICommand? AddDirectorCommand { get; private set; }
+        public ICommand? UpdateDirectorCommand { get; private set; }
 
         public Director? SelectedDirector
         {
@@ -31,15 +30,11 @@ namespace MovieCRUD.ViewModels
             {
                 _selectedDirector = value;
                 OnPropertyChanged(nameof(SelectedDirector));
-                DeleteDirectorCommand.RaiseCanExecuteChanged();
-                UpdateDirectorCommand.RaiseCanExecuteChanged();
             }
         }
 
         public MainViewModel()
         {
-            AddDirectorCommand = new AddDirectorCommand(this);
-            UpdateDirectorCommand = new UpdateDirectorCommand(this);
             Directors = new ObservableCollection<Director>();
             Directors.Add(new Director()
             {
@@ -53,7 +48,62 @@ namespace MovieCRUD.ViewModels
                 Nationality = "UK",
                 YearOfBirth = 1970
             });
-            SelectedDirector = Directors.FirstOrDefault();
+            StartCommands();
+        }
+
+        public void StartCommands()
+        {
+            AddDirectorCommand = new RelayCommand(
+                (object _) =>
+            {
+                Director newDirector = new();
+
+                var addDirectorWindow = new AddDirectorWindow
+                {
+                    DataContext = newDirector
+                };
+
+                addDirectorWindow.ShowDialog();
+
+                if (addDirectorWindow.DialogResult.HasValue && addDirectorWindow.DialogResult.Value)
+                {
+                    Directors.Add(newDirector);
+                    SelectedDirector = newDirector;
+                }
+            });
+
+            UpdateDirectorCommand = new RelayCommand(
+                (object _) =>
+                {
+                    Director directorToUpdate = (Director)SelectedDirector.Clone();
+
+                    UpdateDirectorWindow updateDirectorWindow = new()
+                    {
+                        DataContext = directorToUpdate
+                    };
+
+                    updateDirectorWindow.ShowDialog();
+
+                    if (updateDirectorWindow.DialogResult.HasValue && updateDirectorWindow.DialogResult.Value)
+                    {
+                        CopyProperties.CopyObj(directorToUpdate, SelectedDirector);
+                    }
+
+                },
+                (object _) =>
+                {
+                    return SelectedDirector != null;
+                });
+            DeleteDirectorCommand = new RelayCommand(
+                (object _) =>
+            {
+                Directors.Remove(SelectedDirector);
+                SelectedDirector = Directors.FirstOrDefault();
+            },
+                (object _) =>
+                {
+                    return SelectedDirector != null;
+                });
         }
     }
 }
